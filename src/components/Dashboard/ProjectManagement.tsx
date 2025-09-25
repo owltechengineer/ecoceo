db8fed704041c0df9c82eb69bec20e18fa3187f4
owltechengineer <owltechprint@gmail.com>
2025-09-25 15:27:14 +0200

    Remove: Rimossa card wrapper da ProjectManagement
    ������� CARD WRAPPER RIMOSSA:
    - ��� Div wrapper con padding e spacing
    - ��� Card container
    - ��� Contenuto diretto senza wrapper
    
    ��� BENEFICI:
    - Layout pi�� pulito ���
    - Nessuna card wrapper ���
    - Contenuto diretto ���
    - Interfaccia semplificata ���
    
    La gestione progetti ora �� completamente senza card wrapper! �������

--- src/components/Dashboard/ProjectManagement.tsx
+++ src/components/Dashboard/ProjectManagement.tsx
@@ -4,10 +4,5 @@ import ProjectsWithCards from './ProjectsWithCards';
 
 export default function ProjectManagement() {
 
-  return (
-    <div className="space-y-6 min-h-full p-6">
-      {/* Componente con card */}
-      <ProjectsWithCards />
-    </div>
-  );
+  return <ProjectsWithCards />;
 }
